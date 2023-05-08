9038210b322d9550a33af1d3c15ee505deaaaf78
Noella <49508517+ItsNoellaHere@users.noreply.github.com>
2023-05-07 22:57:41 -0600

    Update the weapon grid background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">Monster Hunter Rise<br/>Progression Wiki</h1>
       </div>
     </header>
   );
@@ -21,7 +21,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={siteConfig.title}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <WeaponGrid />
